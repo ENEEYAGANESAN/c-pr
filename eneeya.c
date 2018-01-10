d2530bb258a1da9bffeae1a815ddc2819249199a
ENEEYAGANESAN <35128023+ENEEYAGANESAN@users.noreply.github.com>
2018-01-10 07:39:46 -0300

    Create eneeya.c

---
+++ eneeya.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the n");
+scanf("%d",&n);
+if(n>=0)
+{
+printf("positive");
+}
+if(n<=0)
+{
+printf("negative");
+}
+if(n=0)
+{
+printf("zero");
+}
+return 0;
+}
